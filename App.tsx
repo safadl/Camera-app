217ecc257c19a0fb4cfb260be3c7eda4086105b7
safad <safadeldoul@gmail.com>
2023-01-29 18:49:09 +0100

    Integrate camera and result component to App

--- App.tsx
+++ App.tsx
@@ -1,11 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Camera } from "expo-camera";
+import { CameraScreen } from "./src/screens/CameraScreen";
 
 export default function App() {
+  const [openCamera, setOpenCamera] = useState(false);
+
+  const launchCamera = async () => {
+    const { status } = await Camera.requestCameraPermissionsAsync();
+
+    if (status === "granted") {
+      setOpenCamera(true);
+    } else {
+      Alert.alert("Permission to open camera denied");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {openCamera ? (
+        <CameraScreen />
+      ) : (
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: "#fff",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <TouchableOpacity onPress={launchCamera} style={styles.button}>
+            <Text style={styles.text}>Launch Camera</Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
   );
 }
@@ -13,8 +41,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  button: {
+    width: 130,
+    borderRadius: 4,
+    backgroundColor: "#02cfb0",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 40,
+  },
+  text: {
+    color: "#fff",
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
